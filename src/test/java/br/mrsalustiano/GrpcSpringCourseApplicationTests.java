d41696ce14f49e111ea081de96e17ea969496545
Marcelo S Silva <mrsalustiano@gmail.com>
2022-11-28 02:57:51 -0300

    Inicio

---
+++ src/test/java/br/mrsalustiano/GrpcSpringCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.mrsalustiano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcSpringCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
